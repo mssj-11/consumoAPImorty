9b019cb2402f76dbf21e67526e8fc179eda6489e
Mike <80673026+mssj-11@users.noreply.github.com>
2023-07-11 16:15:47 -0600

    Config - Componente Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  data: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+      this.llenarData();
+  }
+
+
+  llenarData(){
+    this.apiService.getData().subscribe( data => {
+      this.data = data;
+      console.log('Datos cargados');
+      console.log(this.data);
+    });
+  }
+
 
 }
